4ad70f39f61e2b3f1e124de5c0b48c24cd1f0835
harnoorpreet <145165802+harnoorpreet@users.noreply.github.com>
2024-01-30 21:39:07 -0800

    Folder structure added
    In this initial commit folder structure was added.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//The Window.console property returns a reference to the console object, which provides methods for logging information to the browser's console.
+console.log("JS file connected");
+
+console.log('JavaScript is running!');
+
+const vectorGraphic = document.querySelector('#badge');
+//console.log(vectorGraphic);
+console.log(this);
+
+
+function logThisId(colour) {
+    console.log(this);
+    console.log('clicked on this element:', this.id);
+
+}
+
+
+
+
+vectorGraphic.addEventListener('click', logThisId);
